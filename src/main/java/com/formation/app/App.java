ef2480e72297c26361a52e687a4039110312e64d
Frederic <flaurens@qualixo.com>
2018-05-07 16:43:39 +0200

    third commit

--- src/main/java/com/formation/app/App.java
+++ src/main/java/com/formation/app/App.java
@@ -2,7 +2,7 @@ package com.formation.app;
 
 public class App {
 	
-	private final String message = "Hello World!";
+	private final String message = "Hello World of docker!";
 	
 	    public App() {}
 	
